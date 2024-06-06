4dff12706867563fdc5624152cbcfa40e09b5d00
TayluanSantos <tayluanjesus@gmail.com>
2024-06-06 18:39:34 -0300

    fix: corrigindo o status code da exceção de 200 para 400

--- src/main/java/com/example/api_cep_integracao/exception/exceptions/InvalidParametersException.java
+++ src/main/java/com/example/api_cep_integracao/exception/exceptions/InvalidParametersException.java
@@ -3,7 +3,6 @@ package com.example.api_cep_integracao.exception.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidParametersException extends RuntimeException{
     public InvalidParametersException(String message){
         super(message);
--- src/main/java/com/example/api_cep_integracao/exception/handler/CustomExceptionHandler.java
+++ src/main/java/com/example/api_cep_integracao/exception/handler/CustomExceptionHandler.java
@@ -2,12 +2,15 @@ package com.example.api_cep_integracao.exception.handler;
 
 import com.example.api_cep_integracao.exception.error.ErrorMessage;
 import com.example.api_cep_integracao.exception.exceptions.InvalidParametersException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
     @ExceptionHandler(InvalidParametersException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public ErrorMessage handleInvalidParametersException(InvalidParametersException ex) {
         return new ErrorMessage(ex.getMessage());
     }
